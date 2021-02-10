d223c3c170fb1b5adb748b5dee8c01229899931d
Savio Ferreira de Souza <log.savio@gmail.com>
2021-02-10 19:50:23 -0300

    :q

---
+++ backend_dart/bin/server.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:args/args.dart';
+import 'package:dotenv/dotenv.dart';
+import 'package:shelf/shelf.dart' as shelf;
+import 'package:shelf/shelf_io.dart' as io;
+
+// For Google Cloud Run, set _hostname to '0.0.0.0'.
+const _hostname = '0.0.0.0';
+
+void main(List<String> args) async {
+  var parser = ArgParser()..addOption('port', abbr: 'p');
+  var result = parser.parse(args);
+
+  // For Google Cloud Run, we respect the PORT environment variable
+  var portStr = result['port'] ?? Platform.environment['PORT'] ?? '8080';
+  var port = int.tryParse(portStr);
+
+  if (port == null) {
+    stdout.writeln('Could not parse port value "$portStr" into a number.');
+    // 64: command line usage error
+    exitCode = 64;
+    return;
+  }
+
+  var handler = const shelf.Pipeline()
+      .addMiddleware(shelf.logRequests())
+      .addHandler(_echoRequest);
+
+  var server = await io.serve(handler, _hostname, port);
+  print('Serving at http://${server.address.host}:${server.port}');
+}
+
+shelf.Response _echoRequest(shelf.Request request) {
+  if (request.url.toString() == 'hello') {
+    return shelf.Response.ok('Hello World em Dart no Dart Week!');
+  }
+  return shelf.Response.ok('Request for "${request.url}"');
+}
+
+Future<void> loadConfigApplication() async {
+  await load();
+
+  
+}
