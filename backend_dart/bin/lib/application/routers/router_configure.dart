43477b3ede6e4d25aeb70426fded70e37ca8a76d
Savio Ferreira de Souza <log.savio@gmail.com>
2021-02-11 00:49:59 -0300

    Correção de linhas de codigo

--- backend_dart/bin/lib/application/routers/router_configure.dart
+++ backend_dart/bin/lib/application/routers/router_configure.dart
@@ -1,14 +1,13 @@
 import 'package:shelf_router/src/router.dart';
 
+import '../modules/user/users_routers.dart';
 import 'i_router_configure.dart';
 
 class RouterConfigure {
   final Router _router;
-  final List<IRouterConfigure> routers = [];
+  final List<IRouterConfigure> routers = [UsersRouters()];
 
   RouterConfigure(this._router);
 
-
   void configure() => routers.forEach((r) => r.configure(_router));
-    
 }
