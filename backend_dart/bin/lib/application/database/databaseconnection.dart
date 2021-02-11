dfffbff81c30bb323b857ea08314d0f74f95290b
Savio Ferreira de Souza <log.savio@gmail.com>
2021-02-10 21:00:46 -0300

    Implementado a interface

---
+++ backend_dart/bin/lib/application/database/databaseconnection.dart
@@ -0,0 +1,23 @@
+import 'package:mysql1/src/single_connection.dart';
+import '../config/database_connection_configuration.dart';
+import 'idatabaseonnection.dart';
+
+class DatabaseConnection implements IDatabaseConnection {
+
+  final DatabaseConnectionConfiguration _configuration;
+ DatabaseConnection(this._configuration);
+  @override
+  Future<MySqlConnection> openConnection() {
+  
+    @override
+    Future<MySqlConnection> openConnection() {
+      return MySqlConnection.connect(ConnectionSettings(
+        host: _configuration.host,
+        port: int.parse(_configuration.port),
+        user: _configuration.user,
+        password: _configuration.password,
+        db: _configuration.databaseName,
+      ));
+    }
+  }
+}
